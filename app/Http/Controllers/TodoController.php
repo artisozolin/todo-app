d14bf65f5464c8bd78ce4c7402ed0db7501a7f7a
artisozolin <artisozolins125@gmail.com>
2025-05-07 19:08:11 +0300

    TODO-5 - Add CRUD for Tasks and Add migration files for database

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        return view('todo');
+        $tasks = Task::orderBy('date', 'desc')->get();
+
+        $totalTasks = $tasks->count();
+        $doneTasks = $tasks->where('status', 'Done')->count();
+        $inProgressTasks = $tasks->where('status', 'In progress')->count();
+
+        return view('todo', compact('tasks', 'totalTasks', 'doneTasks', 'inProgressTasks'));
     }
 }
--- database/migrations/2025_05_06_165838_create_users_table.php
+++ database/migrations/2025_05_06_165838_create_users_table.php
@@ -15,11 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('generated_cookie')->unique();
+            $table->date('cookie_date');
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
         });
     }
